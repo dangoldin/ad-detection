8d2f6baa636c4478da69faa7d484ebfc55bc62cf
Dan Goldin <dangoldin@gmail.com>
2017-01-08 23:10:22 -0500

    Get rid of CV code since clicking through now

--- run.py
+++ run.py
@@ -4,13 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-from googleapiclient import discovery
-from googleapiclient import errors
-
-from oauth2client.client import GoogleCredentials
-
 from PIL import Image
 
+import logging
 import time
 import uuid
 import io
@@ -27,21 +23,15 @@ SLEEP_SECONDS = 15
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 800
 
-DISCOVERY_URL = 'https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'  # noqa
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credentials.json'
-
 OUT_DIR = 'out'
+
+logging.basicConfig(level=logging.DEBUG)
 
 class Crawler:
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.driver.set_page_load_timeout(PAGE_LOAD_TIMEOUT_SECONDS)
         self.driver.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
-
-        self.credentials = GoogleCredentials.get_application_default()
-        self.service = discovery.build(
-            'vision', 'v1', credentials=self.credentials,
-            discoveryServiceUrl=DISCOVERY_URL)
 
     def crawl(self):
         ads = []
@@ -57,7 +47,7 @@ class Crawler:
         main_window = self.driver.current_window_handle
 
         for idx, el in enumerate(els):
-            print 'Processing element', idx
+            logging.info('Processing element: {0}'.format(idx))
 
             img_id = str(uuid.uuid4())
 
@@ -96,11 +86,11 @@ class Crawler:
                 time.sleep(2)
 
                 curr_url = self.driver.current_url
-                print 'Current URL', curr_url
+                logging.info('Current URL {0}'.format(curr_url))
 
                 self.driver.close()
             else:
-                print 'Unable to switch to new tab'
+                logging.error('Unable to switch to new tab')
 
             self.driver.switch_to_window(main_window)
 
@@ -111,9 +101,9 @@ class Crawler:
             right = location['x'] + size['width']
             bottom = location['y'] + size['height'] - scroll
 
-            print 'Scroll', scroll
-            print 'Size', size
-            print 'Location', location
+            logging.debug('Scroll: {}'.format(scroll))
+            logging.debug('Size: {}'.format(size))
+            logging.debug('Location: {}'.format(location))
 
             filepath_ad = filepath.replace('.jpg', '-ad.jpg')
 
@@ -130,70 +120,6 @@ class Crawler:
         self.driver.quit()
 
         return ads
-
-    # # From https://github.com/GoogleCloudPlatform/cloud-vision/blob/master/python/text/textindex.py
-    # def detect_text(self, ads, num_retries=3, max_results=6):
-    #     input_filenames = [os.path.join(OUT_DIR, 'screenshot-' + ad['img_id'] + '-2.jpg') for ad in ads]
-
-    #     images = {}
-    #     for filename in input_filenames:
-    #         with open(filename, 'rb') as image_file:
-    #             images[filename] = image_file.read()
-
-    #     batch_request = []
-    #     for filename in images:
-    #         batch_request.append({
-    #             'image': {
-    #                 'content': base64.b64encode(
-    #                         images[filename]).decode('UTF-8')
-    #             },
-    #             'features': [{
-    #                 'type': 'TEXT_DETECTION',
-    #                 'maxResults': max_results,
-    #             }]
-    #         })
-    #     request = self.service.images().annotate(
-    #         body={'requests': batch_request})
-
-    #     try:
-    #         responses = request.execute(num_retries=num_retries)
-    #         if 'responses' not in responses:
-    #             return {}
-    #         text_response = {}
-    #         for filename, response in zip(images, responses['responses']):
-    #             if 'error' in response:
-    #                 print("API Error for %s: %s" % (
-    #                         filename,
-    #                         response['error']['message']
-    #                         if 'message' in response['error']
-    #                         else ''))
-    #                 continue
-    #             if 'textAnnotations' in response:
-    #                 textAnnotations = response['textAnnotations']
-    #             else:
-    #                 textAnnotations = []
-    #             text_response[filename] = textAnnotations
-    #             with open(filename + '.txt', 'w') as f:
-    #                 f.write(json.dumps(textAnnotations, indent=2))
-    #         return text_response
-    #     except errors.HttpError as e:
-    #         print("Http Error for %s: %s" % (filename, e))
-    #     except KeyError as e2:
-    #         print("Key error: %s" % e2)
-
-    #     return ads
-
-    # def identify_company(self, input_filenames):
-    #     annotation_info = {}
-    #     for filename in input_filenames:
-    #         with open(filename, 'r') as f:
-    #             annotation_info[filename] = json.loads(f.read())
-
-    #     for filename in input_filenames:
-    #         text = annotation_info[filename][0]['description']
-
-    #         # TODO: actually figure out how to go from text to company name
-
 
 if __name__ == '__main__':
     c = Crawler()
