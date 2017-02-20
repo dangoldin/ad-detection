5aa7f12ad19b2855be3baed902998b3cec8d6b32
Dan Goldin <dangoldin@gmail.com>
2017-02-19 22:22:15 -0500

    Clean up design a bit to reflect new JSON

--- browse/src/App.js
+++ browse/src/App.js
@@ -9,8 +9,9 @@ class Ad extends React.Component {
         <div className="twitter-account">
           {this.props.twitterAccount}
         </div>
-        <div className="image">
-          <img src={this.props.adUrl} />
+        <div className="images">
+          <img className="ad-screenshot" src={this.props.adScreenshotURL} alt="ad-screenshot" />
+          <img className="page-screenshot" src={this.props.pageScreenshotURL} alt="page-screenshot" />
         </div>
       </li>
     )
@@ -22,30 +23,44 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      ads: []
+      ads: [],
+      twitterAccountOnly: true
     }
+
+    this.toggleTwitterAccountsOnly = this.toggleTwitterAccountsOnly.bind(this)
+  }
+
+  toggleTwitterAccountsOnly(e) {
+    this.setState({ twitterAccountOnly: e.target.checked })
   }
 
   componentDidMount() {
-    // const jsonAdsPath = 'http://localhost:8000/out.json'
     const jsonAdsPath = 'http://sleeping-giants.s3-website-us-east-1.amazonaws.com/out.json'
     axios.get(jsonAdsPath)
       .then(res => {
         const ads = res.data.ads
-        this.setState({ ads });
+        this.setState({ ads: ads });
       });
   }
 
   render() {
     const baseURL = 'http://sleeping-giants.s3-website-us-east-1.amazonaws.com/'
-    const Ads = this.state.ads.map(function(ad, idx) {
-      var adUrl = baseURL + ad.filename
-      var twitterAccount = (ad.ad_info.twitter_account || '').split('/')[0];
+
+    const filteredAds = this.state.twitterAccountOnly ?
+      this.state.ads.filter((ad) => ad.Twitter_Account !== 'None' ) : this.state.ads;
+
+    const Ads = filteredAds.map(function(ad, idx) {
+      const adScreenshot = baseURL + ad.Ad_Screenshot
+      const pageScreenshot = baseURL + ad.Page_Screenshot
+      const twitterAccount = (ad.Twitter_Account || '').split('/')[0];
+      const adURL = ad.URL
 
       return (
         <Ad key={idx}
           twitterAccount={twitterAccount}
-          adUrl={adUrl}
+          adScreenshotURL={adScreenshot}
+          pageScreenshotURL={pageScreenshot}
+          adURL={adURL}
           />
       )
     })
@@ -53,7 +68,15 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to Ad Detector</h2>
+          <h2>Ad detector</h2>
+        </div>
+        <div>
+          <form>
+            Show Twitter Accounts Only &nbsp;
+            <input type="checkbox"
+              checked={this.state.twitterAccountOnly}
+              onChange={this.toggleTwitterAccountsOnly}/>
+          </form>
         </div>
         <div className="App-intro">
           <ul className="ads">
